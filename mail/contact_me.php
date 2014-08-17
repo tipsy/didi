284db5b74af2c821b00e2e360b9d5ba005377429
Vegar Engen <vegar.engen@gmail.com>
2014-08-17 15:25:25 +0200

    mail to kontakt@didi.no

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'vegar@vegaen.no'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'kontakt@didi.no'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Didi Kontaktskjema:  $name";
 $email_body = "Du har mottatt en melding fra didi kontaktskjema.\n\n"."Detaljene:\n\nNavn: $name\n\nEpost: $email_address\n\nTelefon: $phone\n\nMelding:\n$message";
 $headers = "From: noreply@didi.no\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
